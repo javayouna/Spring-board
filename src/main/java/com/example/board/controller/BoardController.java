d1ecabeef2307058fa98ed3003ce7cad2fbd8aef
youna <javayoun@gmail.com>
2022-10-15 19:02:35 +0900

    Initial commit

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -2,9 +2,14 @@ package com.example.board.controller;
 
 import com.example.board.entity.Board;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.board.service.BoardService;
 
@@ -22,14 +27,29 @@ public class BoardController {
     }
     //게시판 글 전송
     @PostMapping("/board/writepro") // http://localhost:8080/board/write
-    public String boardWritePro(Board board){
+    public String boardWritePro(Board board,Model model){
         boardService.write(board);
-        return "";
+
+        model.addAttribute("message","글 작성이 완료되었습니다.");
+        //model.addAttribute("message","글 작성이 실패하였습니다.");
+        model.addAttribute("searchUrl","/board/list");
+
+        return "message";
     }
     //게시글 리스트
     @GetMapping("/board/list")
-    public String boardList(Model model){
-        model.addAttribute("list",boardService.boardList());
+    public String boardList(Model model, @PageableDefault(page=0, size=10, sort ="id", direction = Sort.Direction.DESC) Pageable pageable) {
+       Page<Board> list = boardService.boardList(pageable);
+
+       int nowPage=list.getPageable().getPageNumber()+1;
+       int startPage =Math.max(nowPage -4,1);
+       int endPage=Math.min(nowPage+5,list.getTotalPages());
+       int lastendPage=Math.min(nowPage,list.getTotalPages());
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+        model.addAttribute("lastendPage",lastendPage);
         return "boardlist";
 
     }
@@ -40,5 +60,31 @@ public class BoardController {
         return "boardview";
     }
 
+    //게시글 삭제 http://localhost:8080/board/delete?id=1
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+    //게시글 수정
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id,Model model){
+    model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    //게시글 업데이트
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id,Board board, Model model){
+        Board boardTemp=boardService.boardView(id); //기존 글 보여주지
+        boardTemp.setTitle(board.getTitle()); //제목
+        boardTemp.setContent(board.getContent()); //내용
+        boardService.write(boardTemp);
+
+        model.addAttribute("message","글 수정이 완료되었습니다.");
+        //model.addAttribute("message","글 작성이 실패하였습니다.");
+        model.addAttribute("searchUrl","/board/list");
+        return "message";
+    }
 
 }
--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.example.board.service;
 import com.example.board.entity.Board;
 import com.example.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,11 +21,18 @@ public class BoardService {
 
     }
     //리스트
-    public List<Board> boardList(){
-      return boardRepository.findAll();
+    public Page<Board> boardList(Pageable pageable){
+      return boardRepository.findAll(pageable);
     }
     //상세페이지
     public Board boardView(Integer id){
         return boardRepository.findById(id).get();
     }
+
+    //특정게시물 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
+    }
+
+
 }
